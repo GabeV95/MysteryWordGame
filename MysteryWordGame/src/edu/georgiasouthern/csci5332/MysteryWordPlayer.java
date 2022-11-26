31f2f8cae754b1de3aa6c00a342bd370dc27482a
Gabe Vindas <gabev014@gmail.com>
2022-11-26 11:14:30 -0500

    First commit with player and server files

---
+++ MysteryWordGame/src/edu/georgiasouthern/csci5332/MysteryWordPlayer.java
@@ -0,0 +1,234 @@
+package edu.georgiasouthern.csci5332;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+public class MysteryWordPlayer extends JFrame {
+	private int width, height, playerId, otherPlayer, maxTurns,
+	turnsMade, myPoints, enemyPoints;
+	private Container contentPane;
+	private JTextArea message;
+	private JButton b1, b2, b3, b4;
+	private ClientSideConnection csc;	
+	private int[] values;
+	private String mysteryWord;
+	private String[] words = new String[4];
+	private ArrayList<JButton>buttons = new ArrayList<>();
+	private boolean buttonsEnabled;
+	private JTextArea window;	
+	public MysteryWordPlayer(int w, int h) {
+		width = w;
+		height = h;
+		contentPane = this.getContentPane();
+		message = new JTextArea();
+		b1 = new JButton("1");
+		b2 = new JButton("2");
+		b3 = new JButton("3");
+		b4 = new JButton("4");
+		buttons.add(b1);
+		buttons.add(b2);
+		buttons.add(b3);
+		buttons.add(b4);
+		values = new int[4];
+		turnsMade = 0;
+		myPoints = 0;
+		enemyPoints = 0;
+		
+	}
+	public void setUpGUI() {
+		this.setSize(width, height);
+		this.setTitle("Player #"+playerId);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane.setLayout(new GridLayout(1,5));
+		contentPane.add(message);
+		message.setText("Creating a simple turn-based game in java");
+		message.setWrapStyleWord(true);
+		message.setLineWrap(true);
+		message.setEditable(false);
+		contentPane.add(b1);
+		contentPane.add(b2);
+		contentPane.add(b3);
+		contentPane.add(b4);
+		this.setVisible(true);
+		
+		if(playerId == 1) {
+			message.setText("You are player #1, go first");
+			buttonsEnabled = true;
+			otherPlayer = 2;
+		}
+		else {
+			message.setText("You are player #2, wait for your turn");
+			otherPlayer = 1;
+			buttonsEnabled = false;
+			Thread t = new Thread(new Runnable() {
+				public void run() {
+					updateTurn();
+				}
+			});
+			t.start();
+		}
+		
+		toggleButtons();
+	}
+	public void connectToServer() {
+		csc = new ClientSideConnection();
+	}
+	public void setUpButtons() {
+		ActionListener al = new ActionListener() {
+		public void actionPerformed(ActionEvent ae) {
+			JButton b = (JButton)ae.getSource();
+			int bNum = Integer.parseInt(b.getText());
+			message.setText("You clicked button number: "+bNum+", now wait for player #"+otherPlayer);
+			turnsMade++;
+			System.out.printf("Turns made: %d%n", turnsMade);
+			buttonsEnabled = false;
+			toggleButtons();
+			myPoints+=values[bNum-1];
+			System.out.printf("My points: %d%n", myPoints);
+			csc.sendBtnNum(bNum);
+			
+			if(playerId==2 && turnsMade==maxTurns) {
+				checkWinner();
+			}
+			else {
+				Thread t = new Thread(new Runnable() {
+					public void run() {
+						updateTurn();
+					}
+				});
+				t.start();
+			}
+		}
+	};
+	for(int i = 0;i<buttons.size();i++) {
+		buttons.get(i).addActionListener(al);
+	}
+//	b1.addActionListener(al);
+//	b2.addActionListener(al);
+//	b3.addActionListener(al);
+//	b4.addActionListener(al);
+		}
+	public void toggleButtons() {
+		for(int i = 0;i<buttons.size();i++) {
+			buttons.get(i).setEnabled(buttonsEnabled);
+		}
+	}
+//	public void startReceivingBtnNums() {
+//		Thread t = new Thread(new Runnable() {
+//			public void run() {
+//				while(true) {
+//					csc.receiveBtnNum();
+//				}
+//			}
+//		});
+//		t.start();
+//	}
+	public void updateTurn() {
+		int num = csc.receiveBtnNum();
+		message.setText("Your enemy clicked button #"+num+". Your turn");;
+		enemyPoints+=values[num-1];
+		System.out.printf("Your enemy has %d points%n", enemyPoints);
+		
+		if(playerId==1&&turnsMade==maxTurns) {
+			checkWinner();
+		}
+		else {
+			buttonsEnabled = true;
+		}
+		toggleButtons();
+	}
+	private void checkWinner() {
+		buttonsEnabled = false;
+		if(myPoints>enemyPoints) {
+			message.setText("You won!\nYou: "+myPoints+"\nEnemy: "+enemyPoints);
+		}
+		else if(myPoints<enemyPoints) {
+			message.setText("You lost!\nYou: "+myPoints+"\nEnemy: "+enemyPoints);
+		}
+		else {
+			message.setText("It's a tie. You both got: "+myPoints+"points");
+		}
+		
+		csc.closeConnection();
+	}
+	//Client Connection
+	private class ClientSideConnection{
+		private Socket socket;
+		private DataInputStream dataIn;
+		private DataOutputStream dataOut;
+		
+		public ClientSideConnection() {
+			System.out.println("Client");
+			
+			try {
+				socket = new Socket("localhost", 51736);
+				dataIn = new DataInputStream(socket.getInputStream());
+				dataOut = new DataOutputStream(socket.getOutputStream());
+				playerId = dataIn.readInt();
+				System.out.printf("Connected to server as Player #%d%n", playerId);
+				maxTurns = dataIn.readInt()/2;
+				
+				mysteryWord = dataIn.readUTF();
+				words[0] = dataIn.readUTF();
+				words[1] = dataIn.readUTF();
+				words[2] = dataIn.readUTF();
+				words[3] = dataIn.readUTF();
+				
+//				values[0] = dataIn.readInt();
+//				values[1] = dataIn.readInt();
+//				values[2] = dataIn.readInt();
+//				values[3] = dataIn.readInt();
+				
+				
+//				for(int i : values) {
+//					values[i] = dataIn.readInt();
+//				}
+				System.out.printf("maxTurns: %d%n", maxTurns);
+				
+//				for(int i : values) {
+//					System.out.printf("Value #%d is %d",i, values[i]);
+//				}
+				
+			}catch(IOException ex) {
+				System.out.println("IOException from CSC constructor");
+			}
+		}
+		
+		public void sendBtnNum(int num) {
+			try {
+				dataOut.writeInt(num);
+				dataOut.flush();
+			}catch(IOException ex) {
+				System.out.println("IOException from send button");
+			}
+		}
+		public int receiveBtnNum() {
+			int num = -1;
+			try {
+				num = dataIn.readInt();
+				System.out.printf("Player #%d clicked button #%d%n", otherPlayer, num);
+			}catch(IOException ex) {
+				System.out.println("IOException at receiveBtnNum CSC");
+			}
+			return num;
+		}
+		public void closeConnection() {
+			try {
+				socket.close();
+				System.out.println("Connection closed");
+			}catch(IOException ex) {
+				System.out.println("IOException in closing the connection");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		MysteryWordPlayer p = new MysteryWordPlayer(500, 100);
+		p.connectToServer();
+		p.setUpGUI();
+		p.setUpButtons();
+		
+	}
+}
---
+++ MysteryWordGame/src/edu/georgiasouthern/csci5332/MysteryWordServer.java
@@ -0,0 +1,161 @@
+package edu.georgiasouthern.csci5332;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+public class MysteryWordServer  {
+	private ServerSocket ss;
+	private int numPlayers, turnsMade, maxTurns,
+	player1BtnNum, player2BtnNum, mysteryNum;
+	private ServerSideConnection player1, player2;
+	private int[] values;
+	private String[] guessWords = new String[4];
+	private char mysteryChar;
+	private String mysteryWord;
+	public MysteryWordServer() {
+		System.out.println("Game Server");
+		numPlayers = 0;
+		turnsMade = 0;
+		maxTurns = 4;
+		values = new int[4];
+		
+//		for(int i = 0;i<values.length; i++) {
+//			values[i] = (int)Math.ceil(Math.random() * 26);
+//			System.out.printf("Value #%d is %d%n", i+1, values[i]);
+//		}
+		mysteryNum = (int)Math.ceil((Math.random()*4));
+		
+		try {
+			Scanner scanner = new Scanner(new File("C:/Users/gabev/Downloads/words.txt"));
+			List<String> words = new ArrayList<>();
+		      
+		      while (scanner.hasNext()) {
+		        words.add(scanner.nextLine());
+		      }
+		      
+		      Random rand = new Random();
+		      mysteryWord = words.get(rand.nextInt(words.size()));
+		      
+		      for(int i = 0;i<guessWords.length;i++) {
+		    	  if(mysteryNum==i) {
+		    		  guessWords[i] = mysteryWord;
+		    	  }
+		    	  else {
+		    		  guessWords[i] = words.get(rand.nextInt(words.size()));		    		  
+		    	  }
+		    	  System.out.println(guessWords[i]);
+		      }
+		      System.out.println(mysteryWord);
+			ss = new ServerSocket(51736);
+		}catch(IOException io) {
+			System.out.println("IOException");
+		}
+	}
+	public void acceptConnections() {
+		try {
+			System.out.println("Waiting for connection");
+			while(numPlayers<2) {
+				Socket s = ss.accept();
+				numPlayers++;
+				System.out.printf("Player #%d has connected%n", numPlayers);
+				ServerSideConnection ssc = new ServerSideConnection(s, numPlayers);
+				
+				if(numPlayers==1) {
+					player1 = ssc;
+				}
+				else {
+					player2 = ssc;
+				}
+				Thread t = new Thread(ssc);
+				t.start();
+			}
+			System.out.println("We now have two players");
+		}catch(IOException ex) {
+			System.out.println("IOException from acceptConnections");
+		}
+	}
+	private class ServerSideConnection implements Runnable{
+		private Socket socket;
+		private DataInputStream dataIn;
+		private DataOutputStream dataOut;
+		private int playerId;
+		
+		public ServerSideConnection(Socket s, int id) {
+			socket = s;
+			playerId = id;
+			
+			try {
+				dataIn = new DataInputStream(socket.getInputStream());
+				dataOut = new DataOutputStream(socket.getOutputStream());
+			}catch(IOException ex) {
+				System.out.println("IOException from SSC constructor");
+			}
+		}
+		public void run() {
+			try {
+				dataOut.writeInt(playerId);
+				dataOut.writeInt(maxTurns);
+//				for(int i:values) {
+//					dataOut.writeInt(values[i]);
+//				}
+//				dataOut.writeInt(values[0]);
+//				dataOut.writeInt(values[1]);
+//				dataOut.writeInt(values[2]);
+//				dataOut.writeInt(values[3]);
+				dataOut.writeUTF(mysteryWord);
+				dataOut.writeUTF(guessWords[0]);
+				dataOut.writeUTF(guessWords[1]);
+				dataOut.writeUTF(guessWords[2]);
+				dataOut.writeUTF(guessWords[3]);
+				dataOut.flush();
+				
+				while(true) {
+					if(playerId==1) {
+						player1BtnNum = dataIn.readInt();
+						System.out.printf("Player 1 clicked button #%d%n", player1BtnNum);
+						player2.sendBtnNum(player1BtnNum);
+					}
+					else {
+						player2BtnNum = dataIn.readInt();
+						System.out.printf("Player 2 clicked button #%d%n", player2BtnNum);
+						player1.sendBtnNum(player2BtnNum);
+					}
+					turnsMade++;
+					if(turnsMade==maxTurns) {
+						System.out.println("Max turns have been reached");
+						break;
+					}
+				}
+				player1.closeConnection();
+				player2.closeConnection();
+			}catch(IOException ex) {
+				System.out.println("IOException from run() in SS");
+			}
+		}
+		public void sendBtnNum(int num) {
+			try {
+				dataOut.writeInt(num);
+				dataOut.flush();
+			}catch(IOException ex) {
+				System.out.println("IOException from sendBtnNum SS");
+			}
+		}
+		
+		public void closeConnection() {
+			try {
+				socket.close();
+				System.out.println("Connection closed");
+				
+			}catch(IOException ex) {
+				System.out.println("IOException closing connection");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		MysteryWordServer gs = new MysteryWordServer();
+		gs.acceptConnections();
+	}
+}
